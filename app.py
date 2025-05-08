ef79fd02f1cb6e29aae3144fd573e0acb285bfa1
mitsein1 <77496162+mitsein1@users.noreply.github.com>
2025-05-09 01:44:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def visualize_price(ticker):
     start_date = request.args.get('start_date', '2021-01-01')
     end_date = request.args.get('end_date', '2025-01-01')
     data = data_retrieval.get_historical_data(ticker, start_date, end_date)
-    chart = visualization.plot_chart(data)
-    return jsonify({"chart_url": chart})
+    chart_url = visualization.plot_chart(data)
+    return jsonify({"chart_url": chart_url})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
